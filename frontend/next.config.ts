034ddcc318a5d5be690ebcb18071f7c75c557f21
lukeheard <lukeheard08@gmail.com>
2026-04-11 17:21:50 -0500

    grid view cover fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.gr-assets.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images-na.ssl-images-amazon.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 's.gr-assets.com',
+      }
+    ],
   },
 };
 
